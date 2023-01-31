90c2917bfbdf977649485ba1d9ce6a58c514ba9e
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2023-01-31 09:57:43 +0000

    Update DungeonCrawlMain.py

--- DungeonCrawlMain.py
+++ DungeonCrawlMain.py
@@ -3,12 +3,35 @@ import random
 import sys
 # Define some variables for the game
 max_hp = 20
+C = 0
+
+def drawroom():
+    print("  _____________   ")
+    print("  |           |   ")
+    print("  |           |   ")
+    print("  |   _____   |   ")
+    print("  |   |   |   |   ")
+    print("  |___|___|___|   ")
+    print("  /           \   ")
+    print(" /             \  ")
+    print("/_______________\ \n")  
+
+def generateroom():
+    drawroom()
+    C = randint(1,3)
+    if C == 1:
+        print("It is a stone room \n")
+    elif C == 2:
+        print("It is a wood room \n")
+    else:
+        print("It is a brick room \n")
 
 # Start the game
 print("You are in a dungeon and encounter a monster!")
 
 # Keep playing until the player defeats 10 monsters or is defeated
 while True:
+    generateroom()
     monster_hp = random.randint(1, max_hp)
     player_hp = max_hp
     
