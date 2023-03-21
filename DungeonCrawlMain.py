014922cc9ea4d7ce8bb1f82c0248485ff1d4e1ca
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2023-03-21 09:46:29 +0000

    Update DungeonCrawlMain.py

--- DungeonCrawlMain.py
+++ DungeonCrawlMain.py
@@ -54,6 +54,7 @@ while True:
             else:
                 print("The monster attacks you!\n")
                 player_hp -= random.randint(1, 5)
+            
         elif action.lower() == "h" and player_hp < 20:
             print("\nYou heal yourself! \n")
             heal_amount = random.randint(1, 5)
@@ -61,8 +62,10 @@ while True:
                 player_hp += heal_amount
             else:
                 player_hp = 20
+              
         elif action.lower() == "h" and player_hp > 19:
             print("\nYou are at full health! \n")
+      
         elif action.lower() == "f" :
           break
         else:
